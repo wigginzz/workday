5f8d839afd5047013dccbddc68e97625739b7a0f
zhangjianshe <zhangjsf@enn.cn>
2018-03-26 14:40:14 +0800

    release 0.1 change log file to /var/log/workday.log

--- src/main/java/cn/mapway/workday/api/WorkdayController.java
+++ src/main/java/cn/mapway/workday/api/WorkdayController.java
@@ -5,6 +5,7 @@ import cn.mapway.workday.api.module.CatalogsReq;
 import cn.mapway.workday.api.module.CatalogsResp;
 import cn.mapway.workday.service.WorkdayService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,11 +21,10 @@ public class WorkdayController {
     @Autowired
     WorkdayService workdayService;
 
-    @Doc(value="所有目录")
-    @RequestMapping(value = "/catalogs",method = {RequestMethod.POST})
-    public CatalogsResp catalogs(CatalogsReq req)
-    {
-        CatalogsResp r=workdayService.catalogs(req);
+    @Doc(value = "所有目录")
+    @RequestMapping(value = "/catalogs", method = {RequestMethod.POST})
+    public CatalogsResp catalogs(@RequestBody CatalogsReq req) {
+        CatalogsResp r = workdayService.catalogs(req);
         return r;
 
     }
