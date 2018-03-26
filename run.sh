4f85ef9c63d60cf54b0aa84fe60bdf71827fed31
zhangjianshe <zhangjsf@enn.cn>
2018-03-26 13:58:27 +0800

    release 0.1 add execurite script

---
+++ run.sh
@@ -0,0 +1,33 @@
+#! /bin/sh
+
+#启动方法
+start(){
+ now=`date "+%Y%m%d%H%M%S"`
+ exec java -Xms128m -Xmx2048m -jar /target/workday-1.0-SNAPSHOT.war 5 >"$now"_bidcheck.log &
+ #java -Xms128m -Xmx2048m -jar cmpp.jar 5 > log.log &
+ #tail -f result.log
+}
+#停止方法
+stop(){
+ ps -ef|grep java|awk '{print $2}'|while read pid
+ do
+    kill -9 $pid
+ done
+}
+
+case "$1" in
+start)
+start
+;;
+stop)
+stop
+;;
+restart)
+stop
+start
+;;
+*)
+printf 'Usage: %s {start|stop|restart}\n' "$prog"
+exit 1
+;;
+esac
