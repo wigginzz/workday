82f0711e01a27d1d4fcb00eda85dd6c085df3159
zhangjianshe <zhangjianshe@gmail.com>
2018-04-28 14:26:49 +0800

    修改运行脚本

--- run.sh
+++ run.sh
@@ -7,13 +7,14 @@ start(){
  mkdir -p ~/.workday
  mkdir -p ~/log/
 
- echo $$ > ~/.workday/workday.pid
- exec java -Xms256m -Xmx2048m -jar ./target/workday-1.0-SNAPSHOT.war 5 > ~/log/workday.log &
+ nohup java -Xms256m -Xmx2048m -jar ./target/workday-1.0-SNAPSHOT.war > ~/log/workday.log  2>&1 &
+ echo $! > ~/.workday/workday.pid
+
  tail -f ~/log/workday.log
 }
 #停止方法
 stop(){
- kill -9 ~/.workday/workday.pid
+ kill -9 `cat ~/.workday/workday.pid`
 }
 
 case "$1" in
